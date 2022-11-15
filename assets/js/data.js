d0a403443242a29bafa809dd39b65a7816e127aa
ronlocode <ron910517@gmail.com>
2022-11-15 14:45:17 +0800

    asdfasdf

---
+++ assets/js/data.js
@@ -0,0 +1,102 @@
+/*
+{
+    "name": 店名,
+    "metro_line": 捷運線路,
+    "metro_station": 捷運站,
+    "evaluation": google評價,
+    "address": 地址,
+    "contact": 電話,
+    "business_hour": 營業時間,
+    "rest": 公休日期
+}
+*/
+var data = [
+    {
+        "name": "二月拉麵",
+        "metro_line": "淡水信義線",
+        "metro_station": "淡水站",
+        "evaluation": "4.3",
+        "address": "新北市淡水區中山北路一段70號",
+        "contact": none,
+        "business_hour": [
+            "11:00-14:00", 
+            "17:00-21:00"
+        ] ,
+        "rest": "每週二"
+    },
+    {
+        "name": "樂山窄巷拉麵",
+        "metro_line": "淡水信義線",
+        "metro_station": "北投站",
+        "evaluation": "4.1",
+        "address": "台北市北投區大同街27號",
+        "contact": "02-2892-6265",
+        "business_hour": [
+            "11:00-14:30", 
+            "17:00-21:00"
+        ],
+        "rest": none
+    },
+    {
+        "name": "黑曜麵屋",
+        "metro_line": "淡水信義線",
+        "metro_station": "芝山站",
+        "evaluation": "4.7",
+        "address": "台北市北投區尊賢街206巷2號",
+        "contact": "0979-047-907",
+        "business_hour": [
+            "11:00-21:00"
+        ],
+        "rest": none
+    },
+    {
+        "name": "隱家拉麵士林店",
+        "metro_line": "淡水信義線",
+        "metro_station": "士林站",
+        "evaluation": "4.8",
+        "address": "台北市士林區中山北路五段505巷22號",
+        "contact": "02-2880-1011",
+        "business_hour": [
+            "11:30-14:00", 
+            "16:30-22:00"
+        ],
+        "rest": none
+    },
+    {
+        "name": "山形心心拉麵",
+        "metro_line": "淡水信義線",
+        "metro_station": "劍潭站",
+        "evaluation": "4.4",
+        "address": "台北市士林區小東街25號",
+        "contact": "02-2881-8558",
+        "business_hour": [
+            "16:30-23:59",
+        ],
+        "rest": none
+    },
+    {
+        "name": "黑田屋Kurodaya",
+        "metro_line": "淡水信義線",
+        "metro_station": "民權西路站",
+        "evaluation": "4.7",
+        "address": "台北市中山區錦西街31號",
+        "contact": "02-2393-8893",
+        "business_hour": [
+            none
+        ],
+        "rest": none
+    },
+    {
+        "name": "特濃屋",
+        "metro_line": "淡水信義線",
+        "metro_station": "雙連站",
+        "evaluation": "4.7",
+        "address": "台北市中山區錦西街31號",
+        "contact": "02-2393-8893",
+        "business_hour": [
+            "11:30-15:00", 
+            "17:00-21:00"
+        ],
+        "rest": none
+    },
+]
